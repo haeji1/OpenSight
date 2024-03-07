3bf51df06303545208043c1d8d4278d287c49e69
임태수 <ts980730@naver.com>
2024-03-07 16:02:39 +0900

    :sparkles: feat:소셜로그인 기능 페이지

---
+++ frontend/OpenSight/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <button @click="redirectToOAuth">Login with OAuth Provider</button>
+  </div>
+</template>
+
+<script setup>
+
+function redirectToOAuth() {
+  window.location.href = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=4hSpDvA1GDlGXjqG6sac&redirect_uri=http://192.168.31.25:8080/login/oauth2/code/naver'
+}
+</script>
